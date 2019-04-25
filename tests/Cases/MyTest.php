12d2576c657ae0aa685aa184d49b0b73b16c738c
ShuangYa <896640547@qq.com>
2019-04-25 14:27:05 +0800

    test

--- tests/Cases/MyTest.php
+++ tests/Cases/MyTest.php
@@ -4,8 +4,17 @@ namespace SyTest;
 use PHPUnit\Framework\TestCase;
 
 class MyTest extends TestCase {
-	public function testAll() {
+	public function testEquals() {
 		$this->assertEquals([
+			'a' => 1,
+			'b' => 2
+		], [
+			'b' => 2,
+			'a' => 1
+		]);
+	}
+	public function testSame() {
+		$this->assertSame([
 			'a' => 1,
 			'b' => 2
 		], [
